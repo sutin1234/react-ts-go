49cba16d2fae704690fdb59838fc4960ef345c12
Sutin <poly.dev2018@gmail.com>
2023-09-02 19:55:59 +0700

    fix: load .env current proj

--- go-server/config/config.go
+++ go-server/config/config.go
@@ -17,7 +17,7 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
