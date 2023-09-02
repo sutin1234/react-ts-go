9750fd6ccb5104c601698bab6e8bc05be6b79438
Sutin <poly.dev2018@gmail.com>
2023-09-02 20:37:58 +0700

    fix: load .env current proj

--- go-server/config/config.go
+++ go-server/config/config.go
@@ -18,13 +18,15 @@ type Config struct {
 }
 
 const (
-	projectDirName = ""
+	projectDirName = "go-server"
 )
 
 func LoadConfig() (*Config, error) {
 	re := regexp.MustCompile(`^(.*` + projectDirName + `)`)
 	cwd, _ := os.Getwd()
 	rootPath := re.Find([]byte(cwd))
+
+	fmt.Printf(" CWD: %v\n", cwd)
 
 	err := godotenv.Load(string(rootPath) + `/.env`)
 	if err != nil {
@@ -35,7 +37,7 @@ func LoadConfig() (*Config, error) {
 			}).Fatalf("Problem loading .env file %v", err)
 		}
 	}
-	fmt.Printf("Loaded .enf Success %v/.env\n", rootPath)
+	fmt.Printf("Loaded .enf Success %v/.env\n", string(rootPath))
 
 	cfg := &Config{
 		BaseUrl:    os.Getenv("BASE_URL"),
